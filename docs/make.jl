9a67ade315f2917bad2076114bd4d726968b4f4b
Nicola <nicola.scaramuzzino@yandex.com>
2024-05-01 13:23:02 +0200

    update gitlab url

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,5 @@ using Documenter, VibratoMonteCarlo, Literate
 # OUTPUT = joinpath(@__DIR__, "src")
 # Literate.markdown(EXAMPLE, OUTPUT; documenter = true)
 # makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"]), sitename = "VibratoMonteCarlo", modules = [VibratoMonteCarlo], pages = ["index.md", "getting_started.md", "types.md", "stochproc.md", "parallel_vr.md", "payoffs.md", "metrics.md", "multivariate.md", "intdiffeq.md", "extends.md"])
-makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"], repolink = "https://gitlab.com/rcalxrc08/VibratoMonteCarlo"), repo = "https://gitlab.com/rcalxrc08/VibratoMonteCarlo.git", sitename = "VibratoMonteCarlo", modules = [VibratoMonteCarlo], pages = ["index.md"])
-get(ENV, "CI", nothing) == "true" ? deploydocs(repo = "https://gitlab.com/rcalxrc08/VibratoMonteCarlo.git") : nothing
+makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"], repolink = "https://gitlab.com/rcalxrc08/VibratoMonteCarlo.jl"), repo = "https://gitlab.com/rcalxrc08/VibratoMonteCarlo.jl.git", sitename = "VibratoMonteCarlo", modules = [VibratoMonteCarlo], pages = ["index.md"])
+get(ENV, "CI", nothing) == "true" ? deploydocs(repo = "https://gitlab.com/rcalxrc08/VibratoMonteCarlo.jl.git") : nothing
